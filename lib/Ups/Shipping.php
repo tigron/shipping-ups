a234e867807c48d62f565d1871715195ed2a2cf2
Christophe Gosiau <christophe@tigron.be>
2017-02-17 17:38:33 +0100

    new features: - Cleanup of shipping API (extra methods/variables) - Add 'Paperless' API - Preparation of Shipping API to support the paperless documents via Shipping

--- lib/Ups/Shipping.php
+++ lib/Ups/Shipping.php
@@ -11,6 +11,114 @@ namespace Tigron\Ups;
 class Shipping extends Client {
 
 	/**
+	 * Shipper
+	 *
+	 * @var \Tigron\Ups\Contact $shipper
+	 * @access private
+	 */
+	private $shipper = null;
+
+	/**
+	 * Ship_From
+	 *
+	 * @var \Tigron\Ups\Contact $ship_from
+	 * @access private
+	 */
+	private $ship_from = null;
+
+	/**
+	 * Recipient
+	 *
+	 * @var \Tigron\Ups\Contact $recipient
+	 * @access private
+	 */
+	private $recipient = null;
+
+	/**
+	 * Package
+	 *
+	 * @var array $packages
+	 * @access private
+	 */
+	private $packages = [];
+
+	/**
+	 * Notifications
+	 *
+	 * @var array $notifications
+	 * @access private
+	 */
+	private $notifications = [];
+
+	/**
+	 * Service
+	 *
+	 * @var \Tigron\Ups\Service $service
+	 * @access private
+	 */
+	private $service = null;
+
+	/**
+	 * Set shipper
+	 *
+	 * @access public
+	 * @param \Tigron\Ups\Contact $shipper
+	 */
+	public function set_shipper(\Tigron\Ups\Contact $shipper) {
+		$this->shipper = $shipper;
+	}
+
+	/**
+	 * Set recipient
+	 *
+	 * @access public
+	 * @param \Tigron\Ups\Contact $recipient
+	 */
+	public function set_recipient(\Tigron\Ups\Contact $recipient) {
+		$this->recipient = $recipient;
+	}
+
+	/**
+	 * Set package
+	 *
+	 * @access public
+	 * @param \Tigron\Ups\Package $package
+	 */
+	public function add_package(\Tigron\Ups\Package $package) {
+		$this->packages[] = $package;
+	}
+
+	/**
+	 * Set Service
+	 *
+	 * @access public
+	 * @param \Tigron\Ups\Service $serice
+	 */
+	public function set_service(\Tigron\Ups\Service $service) {
+		$this->service = $service;
+	}
+
+	/**
+	 * Set ship_from
+	 *
+	 * @access public
+	 * @param \Tigron\Ups\Contact $ship_from
+	 */
+	public function set_ship_from(\Tigron\Ups\Contact $ship_from) {
+		$this->ship_from = $ship_from;
+	}
+
+	/**
+	 * Add notification
+	 *
+	 * @access public
+	 * @param \Tigron\Ups\Notification $notification
+	 */
+	public function add_notification(\Tigron\Ups\Notification $notification) {
+		$this->notifications[] = $notification;
+	}
+
+	/**
 	 * Validate an Address to UPS AddressValidation API
 	 *
 	 * @access public
@@ -20,22 +128,37 @@ class Shipping extends Client {
 	 * @param \Tigron\Ups\Service $service
 	 * @return array $response
 	 */
-	public function confirm(\Tigron\Ups\Contact $shipper, \Tigron\Ups\Contact $recipient, $packages, \Tigron\Ups\Service $service, \Tigron\Ups\Contact $ship_from = null, $notifications = []) {
+	public function confirm() {
+
+		if (!isset($this->shipper)) {
+			throw new \Exception('Shipper is not set, use "set_shipper()" to define one');
+		}
+
+		if (!isset($this->ship_from)) {
+			throw new \Exception('Ship_From is not set, use "set_ship_from()" to define one');
+		}
+
+		if (!isset($this->recipient)) {
+			throw new \Exception('Recipient is not set, use "set_recipient()" to define one');
+		}
+
+		if (count($this->packages) == 0) {
+			throw new \Exception('Add package first. Use "add_package()" to add one');
+		}
+
+		if (!isset($this->service)) {
+			throw new \Exception('No service set, use "set_service()" to add one');
+		}
+
+
 		$template = Template::get();
-		$template->assign('shipper', $shipper);
-		if ($ship_from === null) {
-			$template->assign('ship_from', $shipper);
-		} else {
-			$template->assign('ship_from', $ship_from);
-		}
-		$template->assign('recipient', $recipient);
-		$template->assign('packages', $packages);
-		$template->assign('service', $service);
-		if (!is_array($notifications)) {
-			$template->assign('notifications', [ $notifications ]);
-		} else {
-			$template->assign('notifications', $notifications);
-		}
+		$template->assign('shipper', $this->shipper);
+		$template->assign('ship_from', $this->ship_from);
+		$template->assign('recipient', $this->recipient);
+		$template->assign('packages', $this->packages);
+		$template->assign('service', $this->service);
+		$template->assign('notifications', $this->notifications);
+
 		$xml = $template->render('call/ShipConfirm.twig');
 
 		$result = $this->call('ShipConfirm', $xml);
