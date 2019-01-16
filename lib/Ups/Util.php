36b4684e0459223066013f9e35c2a2ad11f032f8
Lionel Laffineur <lionel@tigron.be>
2019-01-16 14:01:42 +0100

    Added some more unsupported characters

--- lib/Ups/Util.php
+++ lib/Ups/Util.php
@@ -19,15 +19,18 @@ class Util {
 		$patterns[ 0] = '/Ć|ć|Č|č/';		$replacements[ 0] = 'C';
 		$patterns[ 1] = '/Ę|ę|Ē|ē/';		$replacements[ 1] = 'E';
 		$patterns[ 2] = '/Ł|ł|Ļ|ļ/';		$replacements[ 2] = 'L';
-		$patterns[ 3] = '/Ń|ń|Ņ|ņ/';		$replacements[ 3] = 'N';
+		$patterns[ 3] = '/Ń|ń|Ņ|ņ|ň|Ň/';	$replacements[ 3] = 'N';
 		$patterns[ 4] = '/Ó|ó]/';			$replacements[ 4] = 'O';
 		$patterns[ 5] = '/Ś|ś|Š|š/';		$replacements[ 5] = 'S';
 		$patterns[ 6] = '/Ź|ź|Ż|ż|Ž|ž/';	$replacements[ 6] = 'Z';
-		$patterns[ 7] = '/Ā|ā/';			$replacements[ 7] = 'A';
+		$patterns[ 7] = '/Ā|ā|ą|Ą/';		$replacements[ 7] = 'A';
 		$patterns[ 8] = '/Ģ|ģ/';			$replacements[ 8] = 'G';
 		$patterns[ 9] = '/Ī|ī/';			$replacements[ 9] = 'I';
 		$patterns[10] = '/Ķ|ķ/';			$replacements[10] = 'K';
 		$patterns[11] = '/Ū|ū/';			$replacements[11] = 'U';
+		$patterns[12] = '/ý|Ý/';			$replacements[12] = 'Y';
+		$patterns[13] = '/ť|Ť/';			$replacements[13] = 'T';
+		$patterns[14] = '/ř|Ř/';			$replacements[14] = 'R';
 		return preg_replace($patterns, $replacements, $in);
 	}
 }
